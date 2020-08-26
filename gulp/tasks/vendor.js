59c428d1ab1a720d42a6d2b3e2eeacf6b997b261
prikhodkin <info@prikhodkin.com>
2020-08-26 11:20:30 +0500

    Исправляет ошибку в таске 'vendor'

--- gulp/tasks/vendor.js
+++ gulp/tasks/vendor.js
@@ -1,7 +1,10 @@
 import paths from "../../config";
 import gulp from "gulp";
+import plumber from "gulp-plumber";
+
 
 gulp.task('vendor', ()=> {
     return gulp.src(paths.vendor.src)
+      .pipe(plumber())
       .pipe(gulp.dest(paths.vendor.dest))
 });
